892e914bdbc00e6e22c62c3a0212283ab54dbcfe
N Com <6270037@johnabbottcollege.net>
2023-07-11 13:52:37 -0400

    Added Constructors/equals,toString,getter,setter methods

--- Helicopter_QuadcopterPackage/Quadcopter.java
+++ Helicopter_QuadcopterPackage/Quadcopter.java
@@ -1,11 +1,58 @@
 package Helicopter_QuadcopterPackage;
 
-public class Quadcopter extends Helicopter { //A Quadcopter, is a Helicopter that additionally has the following: max flying speed (int type), which indicates its maximum moving speed. 
-	
-	int maxFlyingSpeed;
-	
-	
-	
-	
+public class Quadcopter extends Helicopter { // A Quadcopter, is a Helicopter that additionally has the following: max
+												// flying speed (int type), which indicates its maximum moving speed.
+
+	private int maxFlyingSpeed;
+
+	public Quadcopter() {
+		super();
+	}
+
+	public Quadcopter(String brand, double price, int horsepower, int numCylinders, int yearCreation,
+			int passengerCapacity, int maxFlyingSpeed) {
+		super(numCylinders, yearCreation, passengerCapacity, brand, price, horsepower);
+		this.maxFlyingSpeed = maxFlyingSpeed;
+	}
+
+	public Quadcopter(Quadcopter quad) {
+		super.setBrand(quad.getBrand());
+		super.setPrice(quad.getPrice());
+		super.setHorsePower(quad.getHorsePower());
+		super.setCylinders(quad.getCylinders());
+		super.setCreationYear(quad.getCreationYear());
+		super.setPassengerCapacity(quad.getPassengerCapacity());
+		this.maxFlyingSpeed = quad.maxFlyingSpeed;
+	}
+
+	public int getMaxFlyingSpeed() {
+		return maxFlyingSpeed;
+	}
+
+	public void setMaxFlyingSpeed(int maxFlyingSpeed) {
+		this.maxFlyingSpeed = maxFlyingSpeed;
+	}
+
+	@Override
+	public String toString() {
+		return "Quadcopters Brand: " + getBrand() + " | Price: " + getPrice() + " | Horsepower: " + getHorsePower()
+				+ " | Number of Cylinders: " + getCylinders() + " | Passenger Capacity: " + getPassengerCapacity()
+				+ " | Year of Creation: " + getCreationYear() + " | Max Flying Speed: " + maxFlyingSpeed;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+
+		Quadcopter other = (Quadcopter) obj;
+
+		return super.getCreationYear() == other.getCreationYear() && super.getCylinders() == other.getCylinders()
+				&& super.getPassengerCapacity() == other.getPassengerCapacity()
+				&& this.maxFlyingSpeed == other.getMaxFlyingSpeed();
+	}
 
 }
--- MultirotorPackage/Multirotor.java
+++ MultirotorPackage/Multirotor.java
@@ -4,8 +4,57 @@ import Helicopter_QuadcopterPackage.Helicopter;
 
 public class Multirotor extends Helicopter { //  that additionally has the following: number of rotors (int type), which indicates its number of rotors/blades that it has.  
 	
-	int numberOfRotors;
+	private int numberOfRotors;
 	
+	public Multirotor() {
+		super();
+	}
+
+	public Multirotor(String brand, double price, int horsepower, int numCylinders, int yearCreation,
+			int passengerCapacity, int numberOfRotors) {
+		super(numCylinders, yearCreation, passengerCapacity, brand, price, horsepower);
+		this.numberOfRotors = numberOfRotors;
+	}
+
+	public Multirotor(Multirotor quad) {
+		super.setBrand(quad.getBrand());
+		super.setPrice(quad.getPrice());
+		super.setHorsePower(quad.getHorsePower());
+		super.setCylinders(quad.getCylinders());
+		super.setCreationYear(quad.getCreationYear());
+		super.setPassengerCapacity(quad.getPassengerCapacity());
+		this.numberOfRotors = quad.numberOfRotors;
+	}
+
+	public int getNumberOfRotors() {
+		return numberOfRotors;
+	}
+
+	public void setNumberOfRotors(int numberOfRotors) {
+		this.numberOfRotors = numberOfRotors;
+	}
+
+	@Override
+	public String toString() {
+		return "Multirotor Brand: " + getBrand() + " | Price: " + getPrice() + " | Horsepower: " + getHorsePower()
+				+ " | Number of Cylinders: " + getCylinders() + " | Passenger Capacity: " + getPassengerCapacity()
+				+ " | Year of Creation: " + getCreationYear() + " | Number of rotors: " + numberOfRotors;
+	}
 	
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		Multirotor other = (Multirotor) obj;
+		
+		return super.getCreationYear() == other.getCreationYear() && super.getCylinders() == other.getCylinders()
+				&& super.getPassengerCapacity() == other.getPassengerCapacity()
+				&& this.numberOfRotors == other.getNumberOfRotors();
+	}
+
 
 }
