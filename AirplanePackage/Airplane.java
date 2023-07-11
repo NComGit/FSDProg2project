b572e0be3326994f04ad3481fd6709cfe2f7bdd9
KarinaDeVargas <kavp86@gmail.com>
2023-07-11 12:26:44 -0400

    update the airplane and Helicopter

--- AirplanePackage/Airplane.java
+++ AirplanePackage/Airplane.java
@@ -1,11 +1,86 @@
 package AirplanePackage;
 
+import java.util.Objects;
+
 public class Airplane { //An Airplane class, which has the following attributes: a brand (String type), price (double type) and horsepower (int type).  
 	
-	String brand;
-	double price;
-	int horsepower;
+	private String brand;
+	private double price;
+	private int horsepower;
 	
 	
+	//initializing attributes with valid values using constructor - by default constructor
+	public Airplane() {
+		//System.out.println("Airplane created by default constructor.");
+		this.brand = "Embraer";
+		this.price = 15413.33;
+		this.horsepower = 123;
+	}
+	
+	//initializing attributes with valid values using constructor - by parameter constructor
+	public Airplane(String br, double pr, int hp) {
+		//System.out.println("Airplane created by parameter constructor.");
+		this.brand = br;
+		this.price = pr;
+		this.horsepower = hp;
+		
+	}
+	
+	//initializing attributes with valid values using constructor - by copy constructor
+	public Airplane(Airplane a) {
+		//System.out.println("Airplane created by copy constructor.");
+		this.brand = a.brand;
+		this.price = a.price;
+		this.horsepower = a.horsepower;
+	}
+		
+	
+	public String getBrand(){
+		return this.brand;
+	}
+
+	public void setBrand(String brand){
+		this.brand = brand;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public int getHorsePower() {
+		return this.horsepower;
+	}
+
+	public void setHorsePower(int horsepower) {
+		this.horsepower = horsepower;
+	}
+
+	@Override
+	public String toString() {
+		return "This airplane brand is " + brand 
+				+ ", it costs " + price 
+				+ "$, and horsepower is " + horsepower + ".";
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Airplane other = (Airplane) obj;
+		return Objects.equals(brand, other.brand) && horsepower == other.horsepower
+				&& price == other.price;
+	}
+	
+		
+		
 
 }
--- Helicopter_QuadcopterPackage/Helicopter.java
+++ Helicopter_QuadcopterPackage/Helicopter.java
@@ -4,11 +4,82 @@ import AirplanePackage.Airplane;
 
 public class Helicopter extends Airplane { //A Helicopter is an Airplane that additionally has the following: a number of cylinders (int type), a creation year (int type), and a passenger capacity (int type).  
 
-	int cylinders;
-	int creationYear;
-	int passengerCapacity;
+	private int cylinders;
+	private int creationYear;
+	private int passengerCapacity;
 	
+	//initializing attributes with valid values using constructor - by default constructor
+	public Helicopter() {
+		//System.out.println("Helicopter created by default constructor.");
+		super();
+		this.cylinders = 54;
+		this.creationYear = 2020;
+		this.passengerCapacity = 400;
+	}
 	
+	//initializing attributes with valid values using constructor - by parameter constructor
+	public Helicopter(int c, int cy, int pc, String br, double pr, int hp) {
+		//System.out.println("Helicopter created by parameter constructor.");
+		super(br, pr, hp);
+		this.cylinders = c;
+		this.creationYear = cy;
+		this.passengerCapacity = pc;
+		
+	}
+	
+	//initializing attributes with valid values using constructor - by copy constructor
+	public Helicopter(Helicopter h) {
+		//System.out.println("Helicopter created by copy constructor.");
+		super(h);
+		this.cylinders = h.cylinders;
+		this.creationYear = h.creationYear;
+		this.passengerCapacity = h.passengerCapacity;
+	}
+
+	public int getCylinders() {
+		return cylinders;
+	}
+
+	public void setCylinders(int cylinders) {
+		this.cylinders = cylinders;
+	}
+
+	public int getCreationYear() {
+		return creationYear;
+	}
+
+	public void setCreationYear(int creationYear) {
+		this.creationYear = creationYear;
+	}
+
+	public int getPassengerCapacity() {
+		return passengerCapacity;
+	}
+
+	public void setPassengerCapacity(int passengerCapacity) {
+		this.passengerCapacity = passengerCapacity;
+	}
+
+	@Override
+	public String toString() {
+		return "Helicopter [cylinders=" + cylinders + ", creationYear=" + creationYear + ", passengerCapacity="
+				+ passengerCapacity + ", getBrand()=" + getBrand() + ", getPrice()=" + getPrice()
+				+ ", getHorsePower()=" + getHorsePower() + "]";
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Helicopter other = (Helicopter) obj;
+		return creationYear == other.creationYear && cylinders == other.cylinders
+				&& passengerCapacity == other.passengerCapacity;
+	}
 	
 	
 }
